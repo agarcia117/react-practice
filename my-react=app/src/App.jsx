5772141944615a83c271ce363b19da28f6b73d28
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-04-14 19:28:26 -0700

    Completed Lesson 4

--- my-react=app/src/App.jsx
+++ my-react=app/src/App.jsx
@@ -1,12 +1,16 @@
-import Card from "./Card"
-import Button from "./Button";
+// import Card from "./Card"
+// import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   return(
     <>
-      <Button></Button>
+      <Student name="Spongebob" age={30} isStudent={true}></Student>
+      <Student name="Patrick" age={42} isStudent={false}></Student>
+      <Student name="Squidward" age={50} isStudent={false}></Student>
+      <Student name="Sandy" age={27} isStudent={true}></Student>
+      <Student></Student>
     </>
-    
   );
 }
 
