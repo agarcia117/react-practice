147e1bbe387e1f258df3f5d733c6935006422803
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-04-15 00:28:52 -0700

    Completed Lesson 6

--- my-react=app/src/App.jsx
+++ my-react=app/src/App.jsx
@@ -1,12 +1,27 @@
 // import Card from "./Card"
 // import Button from "./Button";
-import UserGreeting from "./UserGreeting";
+// import UserGreeting from "./UserGreeting";
 // import Student from "./Student";
+import List from './List.jsx'
 
 function App() {
+
+  const fruits = [{id: 1, name: "apple", calories: 95},
+                  {id: 2, name: "orange", calories: 45},
+                  {id: 3, name: "banana", calories: 105},
+                  {id: 4, name: "coconut", calories: 159},
+                  {id: 5, name: "pineapple", calories: 37}];
+  
+  const vegetables = [{id: 6, name: "potatoes", calories: 110},
+                  {id: 7, name: "celery", calories: 15},
+                  {id: 8, name: "carrots", calories: 25},
+                  {id: 9, name: "corn", calories: 63},
+                  {id: 10, name: "broccoli", calories: 50}];
+
   return(
     <>
-      <UserGreeting isLoggedIn={true} username="agarcia117"></UserGreeting>
+      {fruits.length > 0 && <List items={fruits} category="Fruits"></List>}
+      {vegetables.length > 0 && <List items={vegetables} category="Vegetables"></List>}
     </>
   );
 }
