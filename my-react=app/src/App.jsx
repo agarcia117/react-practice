10eac366376ec88541dc8d3ca63e9c69d046e4e2
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-04-14 19:16:15 -0700

    Initialize Repo at lesson 4

---
+++ my-react=app/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card"
+import Button from "./Button";
+
+function App() {
+  return(
+    <>
+      <Button></Button>
+    </>
+    
+  );
+}
+
+export default App
---
+++ my-react=app/src/Card.jsx
@@ -0,0 +1,13 @@
+import profilePic from './assets/Dragon_ball.jpg'
+
+function Card() {
+  return (
+    <div className="card">
+        <img className='card-image' src={profilePic} alt="profile picture" />
+        <h2 className='card-title'>Bruh</h2>
+        <p className='card-text'>Practicing for jobs and play games</p>
+    </div>
+  );
+}
+
+export default Card
