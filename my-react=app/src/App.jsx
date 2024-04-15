4ff1867e084d1c0bbdc5408474df7f0906c403bc
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-04-14 23:53:50 -0700

    Completed Lesson 5

--- my-react=app/src/App.jsx
+++ my-react=app/src/App.jsx
@@ -1,15 +1,12 @@
 // import Card from "./Card"
 // import Button from "./Button";
-import Student from "./Student";
+import UserGreeting from "./UserGreeting";
+// import Student from "./Student";
 
 function App() {
   return(
     <>
-      <Student name="Spongebob" age={30} isStudent={true}></Student>
-      <Student name="Patrick" age={42} isStudent={false}></Student>
-      <Student name="Squidward" age={50} isStudent={false}></Student>
-      <Student name="Sandy" age={27} isStudent={true}></Student>
-      <Student></Student>
+      <UserGreeting isLoggedIn={true} username="agarcia117"></UserGreeting>
     </>
   );
 }
