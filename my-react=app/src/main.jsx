43dbb87be16ff874ca2ae512fac31e46dece6a34
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-04-16 11:16:57 -0700

    Completed Lesson 13

--- my-react=app/src/main.jsx
+++ my-react=app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './lesson12/App.jsx'
-import './lesson12/index.css'
+import App from './lesson13/App.jsx'
+import './lesson13/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ my-react=app/src/template/MyComponent.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react"
+
+function MyComponent() {
+  
+  
+
+  return(
+    <div>
+      
+    </div>
+  );
+}
+
+export default MyComponent
