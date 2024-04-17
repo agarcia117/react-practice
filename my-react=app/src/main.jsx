a26d847710e95055f3b0203fbcc921c4a1b8ee4b
Alan Garcia <43732771+agarcia117@users.noreply.github.com>
2024-04-17 14:52:40 -0700

    Completed Lesson 17

--- my-react=app/src/main.jsx
+++ my-react=app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './lesson16/App.jsx'
-import './lesson16/index.css'
+import App from './lesson17/App.jsx'
+import './lesson17/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
